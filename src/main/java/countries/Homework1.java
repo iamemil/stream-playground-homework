9dca89d29217315ef88b9cf883664a35b0e6dd63
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2021-03-23 22:33:10 +0100

    first commit

---
+++ src/main/java/countries/Homework1.java
@@ -0,0 +1,151 @@
+package countries;
+
+import java.io.IOException;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Homework1 {
+
+    private List<Country> countries;
+
+    public Homework1() {
+        countries = new CountryRepository().getAll();
+    }
+
+    /**
+     * Returns whether there is at least one country with the word "island" in its name ignoring case.
+     */
+    public boolean streamPipeline1() {
+        return countries.stream()
+                .anyMatch(c -> c.getName().toLowerCase().contains("island"));
+    }
+
+    /**
+     *  Returns the first country name that contains the word "island" ignoring case.
+     */
+    public Optional<String> streamPipeline2() {
+        return countries.stream()
+                .map(c -> c.getName())
+                .filter(c -> c.toLowerCase().contains("island"))
+                .findFirst();
+    }
+
+    /**
+     * Prints each country name in which the first and the last letters are the same ignoring case.
+     */
+    public void streamPipeline3() {
+        countries.stream()
+                .filter(c -> c.getName().toLowerCase().charAt(0) == c.getName().charAt(c.getName().length()-1))
+                .map(c -> c.getName())
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Prints the populations of the first ten least populous countries.
+     */
+    public void streamPipeline4() {
+        countries.stream()
+                .map(co -> co.getPopulation())
+                .sorted(Comparator.naturalOrder())
+                .limit(10)
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Prints the names of the first ten least populous countries.
+     */
+    public void streamPipeline5() {
+        // TODO
+    }
+
+    /**
+     * Returns summary statistics about the number of country name translations associated with each country.
+     */
+    public IntSummaryStatistics streamPipeline6() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Prints the names of countries in the ascending order of the number of timezones.
+     */
+    public void streamPipeline7() {
+        // TODO
+    }
+
+    /**
+     * Prints the number of timezones for each country in the form {@code name:timezones}, in the ascending order of the number of timezones.
+     */
+    public void streamPipeline8() {
+        // TODO
+    }
+
+    /**
+     * Returns the number of countries with no Spanish country name translation (the Spanish language is identifi
+ed by the language code "es").
+     */
+    public long streamPipeline9() {
+        return countries.stream()
+                .filter(country -> country.getTranslations().containsKey("es")==false)
+                .count();
+    }
+
+    /**
+     * Prints the names of countries with null area.
+     */
+    public void streamPipeline10() {
+        countries.stream()
+                .filter(c -> c.getArea() ==null)
+                .map(c -> c.getName())
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Prints all distinct language tags of country name translations sorted in alphabetical order.
+     */
+    public void streamPipeline11() {
+        // TODO
+    }
+
+    /**
+     * Returns the average length of country names.
+     */
+    public double streamPipeline12() {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Prints all distinct regions of the countries with null area.
+     */
+    public void streamPipeline13() {
+        // TODO
+    }
+
+    /**
+     * Returns the largest country with non-null area.
+     */
+    public Optional<Country> streamPipeline14() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Prints the names of countries with a non-null area below 1.
+     */
+    public void streamPipeline15() {
+        countries.stream()
+                .filter(c -> c.getArea() !=null && c.getArea().intValue()<1)
+                .map(c -> c.getName())
+                .forEach(System.out::println);
+    }
+
+    /**
+     * Prints all distinct timezones of European and Asian countries.
+     */
+    public void streamPipeline16() {
+        // TODO
+    }
+
+}
